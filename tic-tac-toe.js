ceecb4068c5915e9f5a4e07b3070ee0c648a049d
Dianne Le <diannefle@gmail.com>
2017-03-06 17:18:20 -0800

    write naive win logic. fix bug in placing piece in last row

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -32,19 +32,34 @@ Game.prototype.placePiece = function(num) {
     this.Oturn = !this.Oturn;
     this.turn = this.Oturn ? 'o' : 'x';
     this.printLines.call(this);
+    this.checkWinner();
   } else if (this.row1.includes(numStr)) {
     this.row1 = this.row1.replace(numStr, this.turn);
     this.Oturn = !this.Oturn;
     this.turn = this.Oturn ? 'o' : 'x';
     this.printLines.call(this);
+    this.checkWinner();
   } else if (this.row2.includes(numStr)) {
-    this.row2 = this.row1.replace(numStr, this.turn);
+    this.row2 = this.row2.replace(numStr, this.turn);
     this.Oturn = !this.Oturn;
     this.turn = this.Oturn ? 'o' : 'x';
     this.printLines.call(this);
+    this.checkWinner();
   } else {
-    console.log(`Invalid input. Please call game.placePiece(#) on an open number on the board.`)
+    console.log(`Invalid input. Please call game.placePiece(#) on an open number on the board.`);
   }
 };
 
+Game.prototype.checkWinner = function() {
+  var column = this.row0[0] + this.row1[0] + this.row2[0];
+  var majDiag = this.row0[0] + this.row1[1] + this.row2[2];
+  var minDiag = this.row0[2] + this.row1[1] + this.row2[0];
+  if (this.row0 === 'ooo' || column === 'ooo' || majDiag === 'ooo' || minDiag === 'ooo') {
+    this.winner = 'o';
+    console.log('O WINS!');
+  } else if (this.row0 === 'xxx' || column === 'xxx' || majDiag === 'xxx' || minDiag === 'xxx') {
+    this.winner = 'x';
+    console.log('X WINS!');
+  }
+};
 var game = new Game();
